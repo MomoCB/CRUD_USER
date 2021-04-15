f7b50ff4eac6db30b8aad19c2904301297cf0ceb
CB <58017629+MomoCB@users.noreply.github.com>
2021-04-15 00:38:15 -0600

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+
+  firebase:{
+    apiKey: "AIzaSyDyqQ7L2C6ogeXCTTxdRS6K3FmOvLwXZC8",
+    authDomain: "angular8-crud-704e5.firebaseapp.com",
+    databaseURL: "https://angular8-crud-704e5-default-rtdb.firebaseio.com",
+    projectId: "angular8-crud-704e5",
+    storageBucket: "angular8-crud-704e5.appspot.com",
+    messagingSenderId: "197176296010",
+    appId: "1:197176296010:web:4c64c85f59b189858c5cb3"
+  }
+};
